30fe379892dfa48e19efcb18d592b2be7cab334b
AndreyLinyushinIU <a.linyushin@innopolis.university>
2020-11-07 00:12:31 +0300

    07.11.2020-1

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-console.log('aaaaaaaa')
-while(true) {}
+const express = require("express")
+const Router = require("./Router")
+
+const app = express()
+app.use(Router)
+
+async function start() {
+    try {
+        app.listen(process.env.PORT, () => {
+            console.log("Server started");
+        })
+    } catch(e) {
+        console.log(e);
+    }
+}
+
+start().then();
